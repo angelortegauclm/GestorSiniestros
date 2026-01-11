0e6550d00e675ba630b0257947fa714394f7ea4c
CNDA29 <claudiodiaz29100@gmail.com>
2026-01-11 17:04:50 +0100

    Simplificar infraestructura y despliegue

--- src/Notification/EmailFunction.py
+++ src/Notification/EmailFunction.py
@@ -5,6 +5,8 @@ import json
 SNS_TOPIC_ARN = os.environ.get('SNS_TOPIC_ARN')
 sns_client = boto3.client('sns')
 s3_client = boto3.client('s3')
+
+AWS_REGION = os.environ.get('AWS_REGION', 'us-east-1')
  
 def lambda_handler(event, context):
     print("--- INICIANDO NOTIFICACIÓN DE FACTURA ---")
@@ -17,21 +19,16 @@ def lambda_handler(event, context):
         bucket_name = record['s3']['bucket']['name']
         file_key = record['s3']['object']['key']
     
-        presigned_url = s3_client.generate_presigned_url(
-                'get_object',
-                Params={'Bucket': bucket_name, 'Key': file_key},
-                ExpiresIn=86400
-            )
+        direct_url = f"https://{bucket_name}.s3.{AWS_REGION}.amazonaws.com/{file_key}"
     
         siniestro_id = file_key.replace('factura_', '').replace('.pdf', '')
     
         message_text = (
                 f"Estimado cliente,\n\n"
                 f"Le informamos que el informe de tasación para el siniestro '{siniestro_id}' "
-                f"ya ha sido generado y está listo para su descarga.\n\n"
-                f"ACCESO AL DOCUMENTO:\n"
-                f"{presigned_url}\n\n"
-                f"IMPORTANTE: Por motivos de seguridad, este enlace expirará en 24 horas.\n\n"
+                f"ya está disponble para su consulta.\n\n"
+                f"Pulse en el siguiente enlace para visualizar el informe:\n"
+                f"{direct_url}\n\n"
                 f"Atentamente,\n"
                 f"El equipo de Gestión de Siniestros\n"
             )
